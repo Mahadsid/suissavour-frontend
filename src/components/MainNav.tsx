e82ce6dfdde33599611c76e9631a603e18a9ec67
MSID <muhammadmahad0808@gmail.com>
2025-07-28 17:12:26 +0530

    order and manage restraunt ui

--- src/components/MainNav.tsx
+++ src/components/MainNav.tsx
@@ -1,13 +1,21 @@
 import { Button } from './ui/button'
 import { useAuth0 } from '@auth0/auth0-react';
 import UsernameMenu from './UsernameMenu';
+import { Link } from 'react-router-dom';
 
 export default function MainNav() {
   const { loginWithRedirect, isAuthenticated } = useAuth0();
 
   return (
     <span className='flex space-x-2 items-center'>
-      {isAuthenticated ? <UsernameMenu /> :
+      {isAuthenticated ?
+        <>
+          <Link to="/order-status" className='font-bold hover:text-gray-700 hover:cursor-pointer'>
+            Order Staus
+          </Link>
+          <UsernameMenu />
+        </>
+         :
       <Button variant="ghost"
     onClick={async () => await loginWithRedirect() }  className='font-bold text-1xl hover:cursor-pointer'>
           Log In
--- src/components/MobileNav.tsx
+++ src/components/MobileNav.tsx
@@ -1,37 +1,51 @@
-import { Sheet, SheetContent, SheetDescription, SheetTitle, SheetTrigger } from './ui/sheet'
-import { CircleUserRound, Menu } from 'lucide-react'
-import { Separator } from './ui/separator'
-import { Button } from './ui/button'
-import { useAuth0 } from '@auth0/auth0-react'
-import MobileNavLinks from './MobileNavLinks'
+import { CircleUserRound, Menu } from "lucide-react";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetTitle,
+  SheetTrigger,
+} from "./ui/sheet";
+import { Separator } from "./ui/separator";
+import { Button } from "./ui/button";
+import { useAuth0 } from "@auth0/auth0-react";
+import MobileNavLinks from "./MobileNavLinks";
 
+const MobileNav = () => {
+  const { isAuthenticated, loginWithRedirect, user } = useAuth0();
 
-
-export default function MobileNav() {
-    const { isAuthenticated, loginWithRedirect, user } = useAuth0();
   return (
-      <Sheet>
-          <SheetTrigger className='hover: cursor-pointer'>
-              <Menu className='text-black'/>
-          </SheetTrigger>
-          <SheetContent>
-              <SheetTitle className=' text-center text-1xl mt-4'>
-                  {isAuthenticated ?
-                      <span className='flex font-bold items-center  gap-2'>
-                          <CircleUserRound className='text-black' />
-                          {user?.email}
-                      </span>
-                      :
-                      <span>SUISSAVOUR</span>}
-              </SheetTitle>
-              <Separator />
-              <SheetDescription className='flex flex-col gap-4'>
-                  {isAuthenticated ?
-                      <MobileNavLinks />
-                      :
-                      <Button className='flex-1 font-bold bg-gray-800 hover:bg-black' onClick={() => {loginWithRedirect()}}>Log In</Button>}  
-              </SheetDescription>
-          </SheetContent>
+    <Sheet>
+      <SheetTrigger>
+        <Menu className="text-gray-500" />
+      </SheetTrigger>
+      <SheetContent className="space-y-3">
+        <SheetTitle>
+          {isAuthenticated ? (
+            <span className="flex items-center font-bold gap-2">
+              <CircleUserRound className="text-gray-500" />
+              {user?.email}
+            </span>
+          ) : (
+            <span> Welcome to MernEats.com!</span>
+          )}
+        </SheetTitle>
+        <Separator />
+        <SheetDescription className="flex flex-col gap-4">
+          {isAuthenticated ? (
+            <MobileNavLinks />
+          ) : (
+            <Button
+              onClick={() => loginWithRedirect()}
+              className="flex-1 font-bold bg-gray-500"
+            >
+              Log In
+            </Button>
+          )}
+        </SheetDescription>
+      </SheetContent>
     </Sheet>
-  )
-}
+  );
+};
+
+export default MobileNav;
